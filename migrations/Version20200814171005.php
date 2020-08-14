aa8f325270f156d5acac16cb7e455c0c1bee3678
harounaKane <harouna.Kane@hotmail.fr>
2020-08-14 19:24:01 +0200

    version epuree

---
+++ migrations/Version20200814171005.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200814171005 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE article DROP FOREIGN KEY user_fk');
+        $this->addSql('ALTER TABLE categorie CHANGE created_at created_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categorie CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+}
--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Media;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +16,50 @@ class MediaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('legende')
-            ->add('texte')
-            ->add('ordre')
-            ->add('type')
-            ->add('created_at')
-            ->add('article')
+            ->add('nom', TextType::class, [
+                "label" => "Nom du fichier",
+                "attr" => [
+                    "placeholder" => "nom du fichier",
+                    "minlength" => 5,
+                    "maxlength" => 50
+                ]
+            ])
+            ->add('legende', TextType::class, [
+                "label" => "Légende",
+                "attr" => [
+                    "placeholder" => "légende fichier",
+                    "required" => false,
+                    "maxlength" => 255
+                ]
+            ])
+            ->add('texte', TextareaType::class, [
+                "label" => "Description",
+                "attr" => [
+                    "placeholder" => "texte complémentaire du média",
+                    "required" => false,
+                    "maxlength" => 500,
+                    "rows" => 5
+                ]
+            ])
+            ->add('ordre', ChoiceType::class, [
+                "label" => "Ordre",
+                "choices" => [
+                    "1" => 1,
+                    "2" => 2,
+                    "3" => 3,
+                    "4" => 4,
+                    "5" => 5
+                ]
+            ])
+            ->add('type',  ChoiceType::class, [
+                "label" => "Type du média",
+                "choices" => [
+                    "Vidéo" => "video",
+                    "Image" => "image",
+                    "Audio" => "audio"
+                ]
+            ])
+            ->add("Ajouter", SubmitType::class)
         ;
     }
 
